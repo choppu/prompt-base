5cab603a87de05a14548d81cd73ff617f30fef64
Ksenia Lebedeva <ksenia@bitgamma.com>
2025-12-14 09:11:43 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import Prompts from './components/Prompts.vue'
+
+</script>
+
+<template>
+  <header>
+  
+  </header>
+
+  <main>
+    <Prompts/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
