2743f0b3c3f5081fd92823617c5f64da87762e45
Michele Balistreri <michele@bitgamma.com>
2025-12-18 13:50:43 +0100

    add basic about info + license

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-  <p>Hello about</p>
+  <p>Created by: <a href="https://github.com/choppu">@choppu</a> and <a href="https://github.com/choppu">@bitgamma</a>.</p>
+  <p>The original prompts are from <a href="https://github.com/BesianSherifaj-AI/PromptCraft">PromptCraft</a> by <a href="https://github.com/BesianSherifaj-AI">BesianSherifaj-AI</a>. This project wouldn't have been possible without his work.</p>
 </template>
 <style scoped>
 @import '../assets/css/base.css';
